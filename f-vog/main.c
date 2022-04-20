8f18231389a20b87a1280d0fddaff32498e6e92a
Anjala Tamrakar <atamraka@c1r6p6.hive.fi>
2022-04-20 10:59:11 +0300

    first:

---
+++ f-vog/main.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: atamraka <atamraka@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/10 12:51:24 by egaliber          #+#    #+#             */
+/*   Updated: 2022/03/22 21:26:56 by atamraka         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "fillit.h"
+
+int	main(int argc, char **argv)
+{
+	t_item	*piecelist;
+
+	if (argc != 2)
+	{
+		ft_putstr("usage: ./fillit source_file\n");
+		return (0);
+	}
+	piecelist = parser(argv[1]);
+	if (!piecelist)
+	{
+		ft_putstr("error\n");
+		return (0);
+	}
+	solver(piecelist);
+	if (piecelist)
+		free_tet_list (piecelist);
+	return (1);
+}
